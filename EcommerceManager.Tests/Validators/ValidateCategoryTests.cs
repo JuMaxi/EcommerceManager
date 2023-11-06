44a57d912d97a20bc40b3a4655b7ea475f4198b4
JuMaxi <jmaximovski@gmail.com>
2023-11-06 19:16:06 +0000

    CategoryTests

---
+++ EcommerceManager.Tests/Validators/ValidateCategoryTests.cs
@@ -0,0 +1,28 @@
+﻿using EcommerceManager.Interfaces;
+using EcommerceManager.Models.DataBase;
+using EcommerceManager.Validators;
+using FluentAssertions;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceManager.Tests.Validators
+{
+    public class ValidateCategoryTests
+    {
+        [Fact]
+        public void When_name_is_null_should_throw_new_exception()
+        {
+            Category category = new() { Name = null };
+
+            ValidateCategory validatorCategory = new(null);
+
+            validatorCategory.Invoking(validator => validator.Validate(category))
+            .Should().Throw<Exception>()
+            .WithMessage("The Name field must be filled to continue.");
+        }
+    }
+}
--- EcommerceManager/Interfaces/IValidateCategory.cs
+++ EcommerceManager/Interfaces/IValidateCategory.cs
@@ -1,6 +1,9 @@
-﻿namespace EcommerceManager.Interfaces
+﻿using EcommerceManager.Models.DataBase;
+
+namespace EcommerceManager.Interfaces
 {
     public interface IValidateCategory
     {
+        public void Validate(Category category);
     }
 }
