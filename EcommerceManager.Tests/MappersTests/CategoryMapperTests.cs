adff69b5308ad8ea5e6e59fc7e686fdd970f37e7
JuMaxi <jmaximovski@gmail.com>
2023-11-07 16:03:00 +0000

    CategoryMapperTests

--- EcommerceManager.Tests/MappersTests/CategoryMapperTests.cs
+++ EcommerceManager.Tests/MappersTests/CategoryMapperTests.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using EcommerceManager.Mappers;
+using EcommerceManager.Models.DataBase;
+using EcommerceManager.Models.Requests;
+using FluentAssertions;
+using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +13,24 @@ namespace EcommerceManager.Tests.MappersTests
 {
     public class CategoryMapperTests
     {
+        [Fact]
+        public void Checking_If_CategoryRequest_is_Equal_Category()
+        {
+            CategoryRequest categoryRequest = new()
+            {
+                Name = "Trousers",
+                Description = "Women Trousers",
+                Image = "https:myclothes.co.uk/Trousers-Tailored-Business-Straight-Everpress",
+                ParentId = 3,
+            };
+
+            CategoryMapper categoryMapper = new();
+            Category category = categoryMapper.ConvertCategoryRequestToCategory(categoryRequest);
+
+            category.Name.Should().Be(categoryRequest.Name);
+            category.Description.Should().Be(categoryRequest.Description);
+            category.Image.Should().Be(categoryRequest.Image);
+            category.Parent.Id.Should().Be(categoryRequest.ParentId);
+        }
     }
 }
