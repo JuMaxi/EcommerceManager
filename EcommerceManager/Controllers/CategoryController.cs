4d5ec20d07fe2b2a014d62e40c46b1d46bec6307
JuMaxi <jmaximovski@gmail.com>
2023-11-07 16:36:50 +0000

    CategoryAddNew

--- EcommerceManager/Controllers/CategoryController.cs
+++ EcommerceManager/Controllers/CategoryController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace EcommerceManager.Controllers
 {
     [ApiController]
-    [Route("[]Controller")]
+    [Route("[Controller]")]
     public class CategoryController : ControllerBase
     {
         readonly ICategoryService _categoryService;
